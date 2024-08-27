0c3ecc0d106e4e6929b82db95c0e52a196469afa
Huutoan1709 <nguyenhuutoan010@gmail.com>
2024-08-27 22:02:00 +0700

    RoomSerializer 27/8

--- API/serializers.py
+++ API/serializers.py
@@ -1,11 +1,17 @@
 from rest_framework import serializers
-from .models import User,Follow,Rooms,RoomImage,RoomType,Reviews
+from .models import User, Follow, Rooms, RoomImage, RoomType, Reviews, Locations
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from datetime import datetime
 
 
 class UserSerializer(ModelSerializer):
     followed = SerializerMethodField()
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['avatar'] = instance.avatar.url
+        return rep
+
     def get_followed(self, obj):
         if self.context.get('request') and self.context['request'].user.id:
             return Follow.objects.filter(follower=self.context['request'].user, following=obj,
@@ -15,6 +21,7 @@ class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'avatar', 'first_name', 'last_name', 'role', 'followed']
+
 
 class UserInfoSerializer(UserSerializer):
     follower_count = SerializerMethodField()
@@ -42,4 +49,50 @@ class UserInfoSerializer(UserSerializer):
                 {'write_only': True},
             'is_active':
                 {'write_only': True}
+        }
+
+
+class RoomImageSerializer(ModelSerializer):
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['url'] = instance.url.url
+        return rep
+
+    class Meta:
+        model = RoomImage
+        fields = ['id', 'url']
+
+
+class RoomTypeSerializer(ModelSerializer):
+    class Meta:
+        model = RoomType
+        fields = ['name', 'description']
+
+
+class LocationSerializer(ModelSerializer):
+    class Meta:
+        model = Locations
+        fields = ['address', 'city', 'district', 'latitude', 'longitude']
+
+
+class RoomsSerializer(ModelSerializer):
+    is_reserved = SerializerMethodField()
+
+    def get_is_reserved(self, obj):
+        active_booking = Bookings.objects.filter(room=obj, status='CONFIRMED', end_date__gte=timezone.now()).exists()
+        return active_booking
+
+    # images = RoomImageSerializer(many=True, read_only=True, source='Room_Images')
+    # location = LocationSerializer(read_only=True)
+    # room_type = RoomTypeSerializer(read_only=True)
+    # landlord = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Rooms
+        fields = ['id', 'title', 'description', 'price', 'max_people', 'area','status','is_reserved','is_approved']
+                  # 'status', 'landlord', 'location', 'room_type', 'images','is_reserved' ]
+        extra_kwargs = {
+            'landlord': {'read_only': True},
+            'status': {'read_only': True},
+            'is_approved': {'read_only': True},
         }
