5f1743364f37d7e846bb767fdfbddcfda6b3d794
Huutoan1709 <nguyenhuutoan010@gmail.com>
2024-09-11 00:03:55 +0700

    Call API room 9/10

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,8 +1,10 @@
 import Home from "../pages/Home";
 import Profile from "../pages/Profile";
+import Room from "../pages/Room";
 const publicroutes = [
   { path: "/", component: Home },
   { path: "/profile", component: Profile },
+  { path: "/Room", component: Room },
 ];
 const privateRoutes = [];
 export { publicroutes, privateRoutes };
