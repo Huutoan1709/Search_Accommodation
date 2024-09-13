0fbe9591a2ad8854ed8d2e2a4ad3a95d4dd66162
Huutoan1709 <nguyenhuutoan010@gmail.com>
2024-09-13 16:10:08 +0700

    settup redex reducer 13/9

---
+++ frontend/src/reducer/AuthReducer.js
@@ -0,0 +1,11 @@
+const initState = {
+    isLogin: false,
+    token: null,
+};
+const AuthReducer = (state = initState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+export default AuthReducer;
---
+++ frontend/src/reducer/PostReducer.js
@@ -0,0 +1,8 @@
+const initState = {
+    post: [],
+};
+const PostReducer = (initState, action) => {
+    return initState;
+};
+
+export default PostReducer;
